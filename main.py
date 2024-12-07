d3222921e5751da0b4f5d468f4bbd320d5abed84
DanyloPRO <da.kovalenko@gimnasium.te.ua>
2024-12-07 13:26:54 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,226 @@
+name = max
+print (my name is  name)
+
+
+name2 = max
+print(f my mame is mame 2)
+
+
+
+print(f my mame is mame 2)
+print(f name2 word count len(name2)) sumbols)
+
+
+name3 = max
+day = 28
+print(f today is Saturday day:d)
+print(f My mame is name3:10s)
+pi = 3.56439
+print(f Pi number is pi.50.2f)
+income = 20547
+cost = 14489
+profit = income/cost
+print(f Your profit is profit:50.3%)
+day2 = 13
+name4 = "Nick"
+kingOfTheMountain = "Ann"
+print(f"Today is friday, {day2:<15d}, ")
+print(f"{name4:>25}, launched his setup")
+print(f"{kingOfTheMountain:^30} becomes the ling of the mountain. Our congratulations")
+day3 = 14
+month = 10
+/n
+/b
+/t
+/r
+name = Sviat
+print (my name is  name)
+
+
+name2 = Sviat
+print(f my mame is mame 2)
+
+
+
+print(f my mame is mame 2)
+print(f name2 word count len(name2)) sumbols)
+
+
+name3 = sviat
+
+import turtle
+import time
+
+tr = turtle.Turtle()
+tr.shape("turtle")
+
+print(f'\033[31m========Вікторина по тваринам========')
+print('▴◭――◮')
+print("┃ ◉༝◉┃")
+print('┃₌»♥«)')
+print('┃ˍПˍП┃')
+
+print('//інструкція//')
+print('▧!◖відповіді потрібно писати з великої букви◗!▧')
+time.sleep(1)
+print('1')
+time.sleep(1)
+print('2')
+time.sleep(1)
+print('3')
+time.sleep(1)
+print(f'\033[32m ▉▉▉▉▉▉▉▉▉▉▉▉START▉▉▉▉▉▉▉▉▉▉▉▉')
+time.sleep(0.2)
+
+question_1 = 'Без рук, без ніг, а по землі ходить?'
+answer_1 = 'Змія'
+question_2 = 'Двічі родиться, а раз умирає?'
+answer_2 = 'Птах'
+question_3 = 'Є крила, а не літає?'
+answer_3 = 'Риба'
+question_4 = 'Хто мене вб’є, свою кров проллє?'
+answer_4 = 'Комар'
+question_5 = 'Хто з води виходить сухим?'
+answer_5 = 'Качка'
+question_6 = 'Маленьке, сіреньке, а хвостик, як шило'
+answer_6 = 'Миша'
+
+incorrect = 0
+correct = 0
+user_answer = ''
+
+print(question_1 + ' = ?')
+user_answer = input()
+if user_answer == answer_1:
+    print(f'\033[32m Correct ✓')
+correct = correct + 1
+else:
+print(f'\033[31m Incorrect ✘')
+incorrect = incorrect + 1
+print('▉▉▉▉▉▉▉▉▉▉▉▉')
+print('')
+print(question_2 + ' = ?')
+user_answer = input()
+if user_answer == answer_2:
+    print(f'\033[32m Correct ✓')
+correct = correct + 1
+else:
+print(f'\033[31m Incorrect ✘')
+incorrect = incorrect + 1
+print('▉▉▉▉▉▉▉▉▉▉▉▉')
+print('')
+print(question_3 + ' = ?')
+user_answer = input()
+if user_answer == answer_3:
+    print(f'\033[32m Correct ✓')
+correct = correct + 1
+else:
+print(f'\033[31m Incorrect ✘')
+incorrect = incorrect + 1
+print('▉▉▉▉▉▉▉▉▉▉▉▉')
+print('')
+print(question_4 + ' = ?')
+user_answer = input()
+if user_answer == answer_4:
+    print(f'\033[32m Correct ✓')
+correct = correct + 1
+else:
+print(f'\033[31m  Incorrect ✘')
+incorrect = incorrect + 1
+print('▉▉▉▉▉▉▉▉▉▉▉▉')
+print('')
+print(question_5 + ' = ?')
+user_answer = input()
+if user_answer == answer_5:
+    print(f'\033[32m Correct ✓')
+correct = correct + 1
+else:
+print(f'\033[31m Incorrect ✘')
+incorrect = incorrect + 1
+print('▉▉▉▉▉▉▉▉▉▉▉▉')
+print('')
+print(question_6 + ' = ?')
+user_answer = input()
+if user_answer == answer_6:
+    print(f'\033[32m Correct ✓')
+correct = correct + 1
+else:
+print(f'\033[31m Incorrect ✘')
+incorrect = incorrect + 1
+print(f'\033[31m ▉▉▉▉▉▉END▉▉▉▉▉▉')
+
+print(f'\033[32m Кількість прав. відп. - ', correct)
+print(f'\033[31m Кількість неправ. відп. - ', incorrect)
+if correct == 1:
+    print(f'\033[31m капецьььь. нічого незнаєш')
+
+if correct == 2:
+    print(f'\033[31m капецьььь. нічого незнаєш')
+
+if correct == 3:
+    print(f'\033[31m молодееець) але можна було краще')
+
+if correct == 4:
+    print(f'\033[31m молодееець) але можна було краще')
+
+if correct == 5:
+    print(f'\033[33m ще би одиинн, і ти би виграав(ла)')
+
+if correct == 6:
+    print(f'\033[32m МОЛОДЕЕЕЦЬ! усе правильно) ти виграв(ла))) PS переглянь output) то кіт')
+tr.left(50)
+tr.forward(15)
+tr.right(90)
+tr.forward(20)
+tr.left(41)
+tr.forward(20)
+tr.left(50)
+tr.forward(15)
+tr.right(90)
+tr.forward(20)
+tr.left(41)
+tr.right(92)
+tr.forward(70)
+tr.right(90)
+tr.forward(70)
+tr.right(90)
+tr.forward(72)
+tr.right(180)
+tr.penup()
+tr.forward(17)
+tr.left(90)
+tr.forward(35)
+tr.pendown()
+tr.color('black', 'black')
+tr.begin_fill()
+tr.circle(2)
+tr.penup()
+tr.forward(19)
+tr.pendown()
+tr.circle(2)
+tr.end_fill()
+tr.penup()
+tr.right(90)
+tr.forward(9)
+tr.right(90)
+tr.forward(4)
+tr.pendown()
+tr.forward(3)
+tr.penup()
+tr.left(90)
+tr.forward(10)
+tr.pendown()
+tr.circle(6)
+tr.penup()
+tr.left(90)
+tr.forward(19)
+tr.pendown()
+tr.circle(6)
+
+
+
+
+
+
+
+
